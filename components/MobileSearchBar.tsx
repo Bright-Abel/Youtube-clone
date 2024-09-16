e1d27620a833d6aefbc1ff566b148ce9c0687435
Abel Bright <oluwasegunbarnabas@gmail.com>
2024-09-16 11:59:23 +0100

    fix/ add overlay on mobile

---
+++ components/MobileSearchBar.tsx
@@ -0,0 +1,80 @@
+import { Mic, Search, ArrowBack } from '@mui/icons-material';
+
+interface MobileSearchProperties {
+  handleSubmit: (value: React.FormEvent<HTMLFormElement>) => void;
+  searchParams: string | undefined;
+  setSearchParams: (value: string) => void;
+  startListening: () => void;
+}
+const MobileSearchBar: React.FC<MobileSearchProperties> = ({
+  handleSubmit,
+  searchParams,
+  setSearchParams,
+  startListening,
+}) => {
+  return (
+    <div className="fixed inset-0 bg-black z-50">
+      <div className="bg-[#ffffff19] py-4 px-2">
+        <div className="flex gap-3  items-center w-full ">
+          <button
+            type="button"
+            aria-label="Close Search"
+            onClick={() => (window.location.hash = '')}
+            style={{
+              color: '#f1f1f1',
+            }}
+            className="flex justify-center items-center"
+          >
+            <ArrowBack sx={{ width: '24px', height: '24px' }} />
+          </button>
+          <form
+            onSubmit={handleSubmit}
+            className=" h-[40px]  text-[#ffffffe0] flex md:hidden bg-[#303030] rounded-[40px]  flex-1 pl-4"
+          >
+            <input
+              type="text"
+              className="outline-none rounded-[20px] bg-transparent h-full border-none w-full pl-2"
+              value={searchParams}
+              placeholder="Search Videos"
+              onChange={(event) => {
+                setSearchParams(event.target.value);
+              }}
+            />
+            <button
+              type="submit"
+              aria-label="Submit search"
+              style={{
+                color: '#f1f1f1',
+                padding: '10px',
+                //   background: '#ffffff14',
+                borderRadius: '0 40px 40px 0',
+                width: '64px',
+                height: '40px',
+              }}
+              className="flex justify-center items-center"
+            >
+              <Search sx={{ width: '24px', height: '24px' }} />
+            </button>
+          </form>
+          <button
+            type="button"
+            onClick={startListening}
+            aria-label="Start voice search"
+            style={{
+              color: '#fff',
+              padding: '10px',
+              background: '#68686824',
+              width: '40px',
+              height: '40px',
+              borderRadius: '50%',
+            }}
+            className="flex justify-center md:hidden border-[4px] border-solid border-[#3030303b] items-center"
+          >
+            <Mic sx={{ width: '24px', height: '24px' }} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MobileSearchBar;
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,9 @@
 'use client';
-import { IconButton } from '@mui/material';
+
 import { Search, Mic } from '@mui/icons-material';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import MobileSearchBar from './MobileSearchBar';
 
 // Extending Window interface to include SpeechRecognition types
 /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -16,10 +17,29 @@ declare global {
 
 const SearchBar = () => {
   const router = useRouter();
+  const [isSearching, setIsSearching] = useState<boolean>(false);
   const [searchParams, setSearchParams] = useState<string | undefined>(
     undefined
   );
   // const [isListening, setIsListening] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      if (window.location.hash === '#searching') {
+        setIsSearching(true);
+      } else {
+        setIsSearching(false);
+      }
+    };
+
+    handleHashChange();
+
+    window.addEventListener('hashchange', handleHashChange);
+
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+    };
+  });
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -53,48 +73,68 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="flex gap-3 items-center w-full max-w-[600px]">
-      <form
-        onSubmit={handleSubmit}
-        className="border border-solid h-[40px] text-[#ffffffe0] flex bg-[#121212] rounded-[40px] border-[#303030] flex-1 pl-4"
-      >
-        <input
-          type="text"
-          className="outline-none rounded-[20px] caret-red-500 bg-transparent h-full border-none w-full pl-2"
-          value={searchParams}
-          placeholder="Search..."
-          onChange={(event) => {
-            setSearchParams(event.target.value);
-          }}
-        />
-        <IconButton
-          type="submit"
-          sx={{
-            color: '#f1f1f1',
-            p: '10px',
-            background: '#ffffff14',
-            borderRadius: '0 40px 40px 0',
-            width: '64px',
-            height: '40px',
-          }}
+    <div className="w-full max-w-[600px]">
+      <div className="flex gap-3 items-center justify-end lg:justify-normal w-full ">
+        <form
+          onSubmit={handleSubmit}
+          className="border border-solid h-[40px] hidden text-[#ffffffe0] md:flex bg-[#121212] rounded-[40px] border-[#303030] flex-1 pl-4"
         >
-          <Search sx={{ width: '24px', height: '24px' }} />
-        </IconButton>
-      </form>
-      <IconButton
-        type="button"
-        onClick={startListening}
-        sx={{
-          color: '#fff',
-          p: '10px',
-          background: '#ffffff19',
-          width: '40px',
-          height: '40px',
-          borderRadius: '50%',
-        }}
-      >
-        <Mic sx={{ width: '24px', height: '24px' }} />
-      </IconButton>
+          <input
+            type="text"
+            className="outline-none rounded-[20px] caret-red-500 bg-transparent h-full border-none w-full pl-2"
+            value={searchParams}
+            placeholder="Search..."
+            onChange={(event) => {
+              setSearchParams(event.target.value);
+            }}
+          />
+          <button
+            type="submit"
+            style={{
+              color: '#f1f1f1',
+              padding: '10px',
+              background: '#ffffff14',
+              borderRadius: '0 40px 40px 0',
+              width: '64px',
+              height: '40px',
+            }}
+            className="flex justify-center items-center"
+          >
+            <Search sx={{ width: '24px', height: '24px' }} />
+          </button>
+        </form>
+        <button
+          type="button"
+          onClick={startListening}
+          style={{
+            color: '#fff',
+            padding: '10px',
+            background: '#ffffff19',
+            width: '40px',
+            height: '40px',
+            borderRadius: '50%',
+          }}
+          className="md:flex justify-center hidden z-50 items-center"
+        >
+          <Mic sx={{ width: '24px', height: '24px' }} />
+        </button>
+
+        <button
+          type="button"
+          onClick={() => (window.location.hash = 'searching')}
+          className="flex text-[#f1f1f1] md:hidden justify-center items-center"
+        >
+          <Search sx={{ width: '29px', height: '29px' }} />
+        </button>
+      </div>
+      {isSearching && (
+        <MobileSearchBar
+          searchParams={searchParams}
+          setSearchParams={setSearchParams}
+          handleSubmit={handleSubmit}
+          startListening={startListening}
+        />
+      )}
     </div>
   );
 };
